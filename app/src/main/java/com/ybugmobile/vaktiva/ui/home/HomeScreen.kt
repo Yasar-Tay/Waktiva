a7d5733bc7d7fe0d1c340ff5fa5e657a975e6b10
Yasar TAY <yasartay@gmail.com>
2026-01-18 23:10:34 +0100

    before homeScreen UI overhaul

--- app/src/main/java/com/ybugmobile/vaktiva/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ybugmobile/vaktiva/ui/home/HomeScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
