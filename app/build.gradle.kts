c529abc501f63890272d51e3e34652414dc20835
Yasar TAY <yasartay@gmail.com>
2026-04-25 22:56:19 +0200

    version code increased.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ybugmobile.waktiva"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
