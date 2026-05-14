a9dcce5a40693d67bf78ed54bcbb4cfff55766fe
Yasar TAY <yasartay@gmail.com>
2026-05-14 23:30:18 +0200

    increased version number to 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ybugmobile.waktiva"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
