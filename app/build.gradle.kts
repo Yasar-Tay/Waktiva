25a752adb54ab7f15f04a154f3ad3d49ce929d52
Yasar TAY <yasartay@gmail.com>
2026-06-02 12:34:19 +0200

    version 1.0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.ybugmobile.waktiva"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.0.15"
+        versionCode = 16
+        versionName = "1.0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
